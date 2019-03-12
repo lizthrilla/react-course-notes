08effa357435d6dd2fb11fe55907654df6bf4bbd
Liz Tiller <lizthrilla@users.noreply.github.com>
2019-03-12 10:49:05 -0400

    octocat start

--- week1/Octocats/README.md
+++ week1/Octocats/README.md
@@ -1,1 +1,3 @@
 # Octocats
+
+Assignment at: [https://github.com/suncoast-devs/react-part-time-course/blob/master/homework/octocats.md](https://github.com/suncoast-devs/react-part-time-course/blob/master/homework/octocats.md)
--- week1/Octocats/src/App.js
+++ week1/Octocats/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import Cats from './octocats'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <div>
+        <HelloWorld />
+        <img src={Cats.catImage} />
+        <h1>{Cats.catName}</h1>
+      </div>
+    )
   }
 }
 
